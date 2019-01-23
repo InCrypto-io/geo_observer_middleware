509f270149d1ca5b3c8eedd10bd8a01b46ae8ec0
Svyatoslav Delukin <tarzeron@gmail.com>
2019-01-23 14:54:02 +0200

    Chore(config, docker) fix mistake

--- src/config.example.py
+++ src/config.example.py
@@ -1,11 +1,11 @@
 
 # WEB3_PROVIDER = "ws://127.0.0.1:7545"
-WEB3_PROVIDER = "wss://rinkeby.infura.io/ws/v3/your_api_key"
+WEB3_PROVIDER = "wss://rinkeby.infura.io/ws/v3/4086b3432d5941a29b19ae75f0322589"
 Voting_Address = "0x2011Cc6E9B69b6336680F0Ee8A6744b9ae87Af0C"
 VOTING_CREATED_AT_BLOCK = 3660375
 GEOToken_Address = "0xcd99d3da679db4f1a605076028ddfd9b79433918"
 MNEMONIC = "swear picture category mystery together baby off pass shallow one math trophy"
-DB_URL = "mongodb://mongdb:27017/"
+DB_URL = "mongodb://mongodb:27017/"
 # DB_URL = "mongodb://127.0.0.1:27017/"
 CONFIRMATION_COUNT = 30
 INTERVAL_FOR_PREPROCESSED_BLOCKS = 1000
